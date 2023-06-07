3adad82c67420ca35a76da698bc01c1691629528
sangchun1 <sangchunpark18@apu.edu>
2023-06-07 11:14:27 +0900

    finalize recommend_algorithm

--- db/movie.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE movie;
-
-create user 'project'@'%' identified by '1234';
-create user 'project'@'localhost' identified by '1234';
-flush PRIVILEGES;
-
-grant all privileges on movie.* to 'project'@'%';
-grant all privileges on movie.* to 'project'@'localhost';
